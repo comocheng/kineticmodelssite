375ec7ff51b7d55409e1c8870468413f799cd3e6
Richard West <r.west@neu.edu>
2015-07-11 22:55:39 -0400

    Rename 'kineticsdatabase' to 'kineticssite'
    Maybe still not the perfect name, but helps remind me 
    that this is the overall site project, and the other thing 
    (kineticmodels) is an 'app'.

--- kineticssite/wsgi.py
+++ kineticssite/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for kineticsdatabase project.
+WSGI config for kineticssite project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "kineticsdatabase.settings")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "kineticssite.settings")
 
 application = get_wsgi_application()
