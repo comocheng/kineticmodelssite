b153905ac6bc9fb4501305820fda0b0da17a0d65
Richard West <r.west@neu.edu>
2015-07-11 23:44:38 -0400

    Make tests.py not crash
    Not sure it tests anything yet, but it does something now,
    and it looks like it modified the database.

--- kineticmodels/tests.py
+++ kineticmodels/tests.py
@@ -2,13 +2,15 @@ from django.test import TestCase
 
 # Create your tests here.
 
-
 """How to add existing species to an existing reaction"""
 from kineticmodels.models import Kinetics, Reaction, Stoichiometry, Species
+
 r1=Reaction(rPrimeID='r100')
 r1.save()
 s1=Species(sPrimeID='s200',formula='H2O',names='water',thermos=100)
 s2=Species(sPrimeID='s201',formula='CO2',names='carbon dioxide',thermos=200)
+s1.save()
+s2.save()
 Reaction.objects.all()
 Species.objects.all()
 r1 = Reaction.objects.get(id=1) #optional
@@ -25,5 +27,5 @@ s1.reaction_set.all()
 
 """How to link a ForeignKey to its category"""
 #one reaction has multiple different kinetics
-r1.kinetics_set.create(Avalue=1000,Evalue=3000)
+r1.kinetics_set.create(A_value=1000, n_value=0, E_value=3000)
 #if a TypeError appears requesting a string (you provided a float), ignore it and check on the website if the kinetics was successfully added
