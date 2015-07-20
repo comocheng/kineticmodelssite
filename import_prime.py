6db6d2392fe825658bdf198a270913920c39c016
Richard West <r.west@neu.edu>
2015-07-20 17:30:11 -0400

    Fix prime id prefix

--- import_prime.py
+++ import_prime.py
@@ -112,7 +112,7 @@ class SpeciesImporter(Importer):
     def import_elementtree_root(self, species):
         ns = self.ns
         primeID = species.attrib.get("primeID")
-        dj_item, created = Source.objects.get_or_create(bPrimeID=primeID)
+        dj_item, created = Source.objects.get_or_create(sPrimeID=primeID)
         print list(species)
         identifier = species.find('prime:chemicalIdentifier', namespaces=ns)
         for name in identifier.findall('prime:name', namespaces=ns):
