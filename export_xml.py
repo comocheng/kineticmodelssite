9d5a8f89cb131cf34d2ef98e11f9b36125003998
David Green <david.ma.green@gmail.com>
2015-08-04 12:11:45 -0400

    Removed required CAS, formula, inchi
    If do not exist (are None), code will pass on to other children. Also, commented out NIST sources and copyrights, since these will be irrelevant when copying from Django database.

--- export_xml.py
+++ export_xml.py
@@ -7,6 +7,7 @@ and create PrIMe-compatible xml files for each object
 """
 
 from lxml import etree
+import string
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "kineticssite.settings")
 import django
@@ -32,16 +33,16 @@ class xmlSpecies():
         root.attrib["{" + xsi + "}schemaLocation"] = schemaLocation
         # root.attrib["{" + xmlns + "}xsi"] = xsi
         root.attrib["primeID"] = primeID
-        child1=etree.SubElement(root,'copyright')
-        child1.text="primekinetics.org 2005"
+#         child1=etree.SubElement(root,'copyright')
+#         child1.text="primekinetics.org 2005"
         bibliography="b00014319"
-        copyrighted="true"
-        source="NIST"
+#         copyrighted="true"
+#         source="NIST"
         child2 = etree.SubElement(root, 'content')
         child2.attrib["bibliography"] = bibliography
-        child2.attrib["copyrighted"] = copyrighted
-        child2.attrib["source"] = source
-        child2.text="\nElements attributed to NIST are part of a collection copyrighted by NIST.\n"
+#         child2.attrib["copyrighted"] = copyrighted
+#         child2.attrib["source"] = source
+#         child2.text="\nElements attributed to NIST are part of a collection copyrighted by NIST.\n"
         group="prime"
         type="formula"
         child3=etree.SubElement(root, 'preferredKey')
@@ -49,39 +50,43 @@ class xmlSpecies():
         child3.attrib["type"]=type
         child3.text="OH"
         child4=etree.SubElement(root, 'chemicalIdentifier')
-        type="CASRegistryNumber"
-        child41=etree.SubElement(child4, 'name')
-        child41.attrib["source"] = source
-        child41.attrib["type"]=type
-        child41.text="3352-57-6"
+        if CAS is not None:
+            type="CASRegistryNumber"
+            child41=etree.SubElement(child4, 'name')
+    #         child41.attrib["source"] = source
+            child41.attrib["type"]=type
+            child41.text="3352-57-6"
+        if formula is not None:
         type="formula"
-        child42=etree.SubElement(child4, 'name')
-        child42.attrib["source"] = source
-        child42.attrib["type"]=type
-        child42.text="HO"
+#         child42=etree.SubElement(child4, 'name')
+# #         child42.attrib["source"] = source
+#         child42.attrib["type"]=type
+#         child42.text="HO
         child43=etree.SubElement(child4, 'name')
-        child43.attrib["source"] = source
+#         child43.attrib["source"] = source
         child43.text="&middot;OH"
         child44=etree.SubElement(child4, 'name')
-        child44.attrib["source"] = source
+#         child44.attrib["source"] = source
         child44.text="hydroxy radical"
         child45=etree.SubElement(child4, 'name')
-        child45.attrib["source"] = source
+#         child45.attrib["source"] = source
         child45.text="hydroxyl"
         child46=etree.SubElement(child4, 'name')
-        child46.attrib["source"] = source
+#         child46.attrib["source"] = source
         child46.text="hydroxyl radical"
         child47=etree.SubElement(child4, 'name')
-        child47.attrib["source"] = source
+#         child47.attrib["source"] = source
         child47.text="oh"
         child48=etree.SubElement(child4, 'name')
         child48.text="hidroksil"
         child49=etree.SubElement(child4, 'name')
         child49.text="hidroksi radikal"
-        type="InChI"
-        child410=etree.SubElement(child4, 'name')
-        child410.attrib["type"]=type
-        child410.text="InChI=1/HO/h1H"
+        if inchi is not None:
+            type="InChI"
+            child410=etree.SubElement(child4, 'name')
+            child410.attrib["type"]=type
+            child410.text="InChI=1/HO/h1H"
+        
         child5=etree.SubElement(root, 'chemicalComposition')
         atomdict={}
         for atom in mol.atoms:
