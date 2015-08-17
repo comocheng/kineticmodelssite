583bd29ead450b57ab7695b267a6c94811b06dbb
David Green <david.ma.green@gmail.com>
2015-08-17 17:33:38 -0400

    Removed excess Reaction writer stuff

--- export_xml.py
+++ export_xml.py
@@ -308,41 +308,32 @@ class xmlReaction():
     
     def print_reaction_xml(self):
         xmlns="http://purl.org/NET/prime/"
-        xsi="http://www.w3.org/2001/XMLSchema-instance"
+        xsi="http://www.w3.org/2001/XMLSchema_instance"
         rPrimeID="r00000011"
         schemaLocation="http://warehouse.primekinetics.org/schema/reaction.xsd"
-        NSMAP = {None: xmlns}
+        NSMAP = {None: xmlns, 'xsi': xsi}
         root = etree.Element('{' + xmlns + '}reaction', nsmap=NSMAP)
         root.attrib["{" + xsi + "}schemaLocation"] = schemaLocation
         # root.attrib["{" + xmlns + "}xsi"] = xsi
         root.attrib["primeID"] = rPrimeID
-#         child1=etree.SubElement(root,'copyright')
-#         child1.text="primekinetics.org 2005"
         child2=etree.SubElement(root,'reactants')
+        specieslist=['H2O','CO2','H3O2','CH4']
+        sPrimeIDs=['s00000001','s00000002','s00000003','s00000004'] #each element corresponds to specieslist element
+        stoichs=[1,1,-1,-1] #each element corresponds to specieslist element
         stoichdict={}
         for n in range(len(specieslist)):
-            stoichdict["child2-{0}".format(n)]=etree.SubElement(child2,'speciesLink')
-            stoichdict["child2-{0}".format(n)].attrib['preferredKey']=specieslist[n]
-            stoichdict["child2-{0}".format(n)].attrib['primeID']=specieslist[n]<-sPrimeID
-            stoichdict["child2-{0}".format(n)].text=specieslist[n]<-Stoichiometry
-#         preferredKey="C10H11"
-#         primeID="s00000275"
-#         child21=etree.SubElement(child2,'speciesLink')
-#         child21.attrib["preferredKey"]=preferredKey
-#         child21.attrib["primeID"]=primeID
-#         child21.text="-1"
-#         primeID="s00000276"
-#         child22=etree.SubElement(child2,'speciesLink')
-#         child22.attrib["preferredKey"]=preferredKey
-#         child22.attrib["primeID"]=primeID
-#         child22.text="1"
-        print etree.tostring(root, pretty_print=True)
+            stoichdict["child2_{0}".format(n)]=etree.SubElement(child2,'speciesLink')
+            stoichdict["child2_{0}".format(n)].attrib['preferredKey']=specieslist[n]
+            stoichdict["child2_{0}".format(n)].attrib['primeID']=sPrimeIDs[n]
+            stoichdict["child2_{0}".format(n)].text=str(stoichs[n])
+        with open(rPrimeID+'.xml', "w+") as file:
+            file.write(etree.tostring(root, pretty_print=True))
 
 class xmlKinetics():
     
     def print_kinetics_xml(self):
         xmlns="http://purl.org/NET/prime/"
-        xsi="http://www.w3.org/2001/XMLSchema-instance"
+        xsi="http://www.w3.org/2001/XMLSchema_instance"
         rkPrimeID="rk00010102"
         schemaLocation="http://warehouse.primekinetics.org/schema/reactionRate.xsd"
         NSMAP = {None: xmlns, 'xsi': xsi}
