52a1db9dec54d802e327e75a9ecbb763f874ace6
Richard West <r.west@neu.edu>
2015-11-07 21:41:10 -0500

    Added a bibliography view
    Not sure it functions properly

--- kineticmodels/urls.py
+++ kineticmodels/urls.py
@@ -2,7 +2,8 @@ from django.conf.urls import include, url
 from . import views
 
 urlpatterns=[
-    url(r'^$',views.index,name='index'),
+    url(r'^$', views.index, name='index'),
+    url(r'^bibliography/$', views.bibliography, name='bibliography')
 # #     url(r'^(?P<question_id>[0-9]+)/$', views.detail, name='detail'),
 # #     url(r'^(?P<question_id>[0-9]+)/results/$', views.results, name='results'),
 # #     url(r'^(?P<question_id>[0-9]+)/vote/$', views.vote, name='vote'),
--- kineticmodels/views.py
+++ kineticmodels/views.py
@@ -1,7 +1,17 @@
-from django.shortcuts import render
+from django.shortcuts import render, render_to_response, get_object_or_404
 from django.http import HttpResponse
 from django.template import RequestContext, loader
+
+from models import Source
 
 def index(request):
 #     template=loader.get_template('kineticmodels/index.html')
     return HttpResponse("This is the kinetic models index!")
+
+def bibliography(request):
+    """
+    The listing of all the sources in the database
+    """
+    sources = Source.objects.all()
+    variables = {'sources': sources}
+    return render_to_response('bibliography.html', variables, context_instance=RequestContext(request))
