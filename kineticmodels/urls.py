ba6a68dc94c88d1b2b996e3a88c7025917aeecb2
Richard West <r.west@neu.edu>
2015-11-09 14:29:03 -0700

    Added a source editor

--- kineticmodels/urls.py
+++ kineticmodels/urls.py
@@ -4,7 +4,8 @@ from . import views
 urlpatterns=[
     url(r'^$', views.index, name='index'),
     url(r'^bibliography/$', views.bibliography, name='bibliography'),
-    url(r'^(?P<source_id>[0-9]+)/$', views.source, name='source'),
+    url(r'^source/(?P<source_id>[0-9]+)/$', views.source, name='source'),
+    url(r'^source/(?P<source_id>[0-9]+)/edit/$', views.source_editor, name='source editor'),
 # #     url(r'^(?P<question_id>[0-9]+)/results/$', views.results, name='results'),
 # #     url(r'^(?P<question_id>[0-9]+)/vote/$', views.vote, name='vote'),
 ]
--- kineticmodels/views.py
+++ kineticmodels/views.py
@@ -1,7 +1,9 @@
 from django.shortcuts import render, render_to_response, get_object_or_404
-from django.http import HttpResponse
+from django.http import HttpResponse, HttpResponseRedirect
 from django.template import RequestContext, loader
+from django.core.urlresolvers import reverse
 
+from forms import EditSourceForm
 from models import Source
 
 def index(request):
@@ -23,3 +25,23 @@ def source(request, source_id=0):
     source = get_object_or_404(Source, id=source_id)
     variables = {'source': source}
     return render_to_response('kineticmodels/source.html', variables, context_instance=RequestContext(request))
+
+def source_editor(request, source_id=0):
+    """
+    Edit the details of a source
+    """
+    source = get_object_or_404(Source, id=source_id)
+    if request.method == 'POST':
+        form = EditSourceForm(request.POST, instance=source)
+        if form.is_valid():
+            # Save the form
+            form.save()
+            # Go back to the network's main page
+            return HttpResponseRedirect(reverse(source, args=(source.id,)))
+    else:
+        # Create the form
+        form = EditSourceForm(instance=source)
+    variables = {'source': source,
+                 'form': form, }
+    return render_to_response('kineticmodels/source_editor.html', variables, context_instance=RequestContext(request))
+
