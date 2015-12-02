85664eaae7605c13d63be7ce90faa4d3d74eb5cc
nick-rc <nick.rcraig@gmail.com>
2015-12-02 00:28:56 -0500

    Added secretsettings file need to update URLconfig

---
+++ rmgweb/secretsettings.py
@@ -0,0 +1,33 @@
+import os.path
+from rmgpy import settings
+
+DEBUG = True
+TEMPLATE_DEBUG = DEBUG
+
+# The website administrators
+ADMINS = [
+    ('Josh Allen', 'jwallen@mit.edu')
+    # ('Your Name', 'your_email@domain.com'),
+]
+
+# The full path of the Django project (as determined from the location of this file)
+PROJECT_PATH = os.path.realpath(os.path.dirname(__file__))
+
+# The path to the RMG-database project
+DATABASE_PATH = settings['database.directory']
+
+# Configuration of the database backend
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',                 # Add 'postgresql_psycopg2', 'postgresql', 'mysql', 'sqlite3' or 'oracle'.
+        'NAME': os.path.join(PROJECT_PATH, 'sqlitedb', 'rmgweb.db'),        # Or path to database file if using sqlite3.
+        'USER': '',                      # Not used with sqlite3.
+        'PASSWORD': '',                  # Not used with sqlite3.
+        'HOST': '',                      # Set to empty string for localhost. Not used with sqlite3.
+        'PORT': '',                      # Set to empty string for default. Not used with sqlite3.
+    }
+}
+
+# Place your secret key here.
+# Make this unique, and don't share it with anybody.
+SECRET_KEY = 'i+a0dzd@buc8k!4#(zn!*h#f4iinp*_v@6cu!08u_3^uwbzwp#'
