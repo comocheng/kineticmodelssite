4a927b04a54213028eb7aed8441f3f690262d982
Hamza Khan <hamzakhan@CoMoChEng-iMac-1.local>
2016-06-17 12:12:24 -0400

    Added views for individual species, reactions and products.

--- kineticmodels/urls.py
+++ kineticmodels/urls.py
@@ -7,12 +7,16 @@ urlpatterns=[
     url(r'^bibliography/$', views.bibliography, name='bibliography'),
     url(r'^source/(?P<source_id>[0-9]+)/$', views.source, name='source'),
     url(r'^source/(?P<source_id>[0-9]+)/edit/$', views.source_editor, name='source editor'),
-    url(r'^species/page/(?P<pageNumber>[0-9]+)/$', views.species_list, name='species list'),
+    url(r'^species/$', views.species_list, name='species list'),
+    url(r'^species/(?P<species_id>[0-9]+)/$', views.species, name='species'),    
     url(r'^species/(?P<species_id>[0-9]+)/edit/$', views.species_editor, name='species editor'),
     url(r'^models/$', views.kineticModel_list, name='kinetic model list'),
+    url(r'^models/(?P<kineticModel_id>[0-9]+)/$', views.kineticModel, name='kinetic model'),
     url(r'^models/(?P<kineticModel_id>[0-9]+)/edit/$', views.kineticModel_editor, name='kinetic model editor'), 
     url(r'^reactions/$', views.reaction_list, name='reaction list'),
+    url(r'^reactions/(?P<reaction_id>[0-9]+)/$', views.reaction, name='reaction'),    
     url(r'^reactions/(?P<reaction_id>[0-9]+)/edit/$', views.reaction_editor, name='reaction editor'),       
 # #     url(r'^(?P<question_id>[0-9]+)/results/$', views.results, name='results'),
 # #     url(r'^(?P<question_id>[0-9]+)/vote/$', views.vote, name='vote'),
+## page/(?P<pageNumber>[0-9]+)/$
 ]
--- kineticmodels/views.py
+++ kineticmodels/views.py
@@ -2,12 +2,13 @@ from django.shortcuts import render, render_to_response, get_object_or_404
 from django.http import HttpResponse, HttpResponseRedirect
 from django.template import RequestContext, loader
 from django.core.urlresolvers import reverse
+from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 
 from forms import EditSourceForm, EditSpeciesForm, EditReactionForm, EditKineticModelForm
 from models import Source, Species, KineticModel, Reaction
 import math
 
-ITEMSPERPAGE = 10
+ITEMSPERPAGE = 20
 
 def index(request):
 #     template=loader.get_template('kineticmodels/index.html')
@@ -18,13 +19,26 @@ def bibliography(request):
     The listing of all the sources in the database
     """
     sources = Source.objects.all()
-    variables = {'sources': sources}
+
+    paginator = Paginator(sources, ITEMSPERPAGE)
+
+    page = request.GET.get('page')
+    try:
+        sourcesOnAPage = paginator.page(page)
+    except PageNotAnInteger:
+        # If page is not an integer, deliver first page.
+        sourcesOnAPage = paginator.page(1)
+    except EmptyPage:
+        # If page is out of range (e.g. 9999), deliver last page of results.
+        sourcesOnAPage = paginator.page(paginator.num_pages)
+
+    variables = {'sources': sourcesOnAPage}
     return render(request, 'kineticmodels/bibliography.html', variables)
 
 """ See source.html"""
 def source(request, source_id=0):
     """
-    The listing of all the sources in the database
+    The listing of a specific source in the database
     """
     source = get_object_or_404(Source, id=source_id)
     variables = {'source': source}
@@ -58,36 +72,30 @@ def species_list(request, pageNumber = 1):
     """
     species_list = Species.objects.all()
 
-    page = pageGenerator(species_list, pageNumber)
+    paginator = Paginator(species_list, ITEMSPERPAGE)
 
-    variables = {'species_list': page[0], 'currentPage' : page[1], 
-                    'nextPage': page[1]+1, 'previousPage' : page[1]-1,
-                        'totalPages' : page[2],}
+    page = request.GET.get('page')
+    try:
+        speciesOnAPage = paginator.page(page)
+    except PageNotAnInteger:
+        # If page is not an integer, deliver first page.
+        speciesOnAPage = paginator.page(1)
+    except EmptyPage:
+        # If page is out of range (e.g. 9999), deliver last page of results.
+        speciesOnAPage = paginator.page(paginator.num_pages)
+ 
+    variables = {'species_list': speciesOnAPage,}
     return render(request, 'kineticmodels/species_list.html', variables)
 
-def pageGenerator(items, pageNumber):
+""" See species.html"""
+def species(request, species_id=0):
     """
-    Helper function to generate the variables to be displayed on the page 
-    and other information like page number of the page and total number 
-    of pages
+    The listing of a specific species in the database
     """
-    itemsPerPage = ITEMSPERPAGE
-    totalPages = int(math.floor(len(items)/itemsPerPage))+1
+    species = get_object_or_404(Species, id=species_id)
+    variables = {'species': species}
+    return render(request, 'kineticmodels/species.html', variables)
 
-    pageNumber = int(pageNumber)
-
-    if pageNumber == 0:
-        pageNumber = totalPages
-    if pageNumber == totalPages+1:
-        pageNumber = 1
-
-    startIndex = (pageNumber-1)*itemsPerPage
-    endIndex = pageNumber*itemsPerPage    
-    
-    items = items[startIndex:endIndex]
-
-    
-    return [items, pageNumber, totalPages]
 
 def species_editor(request, species_id = 0):
     """
@@ -110,6 +118,8 @@ def species_editor(request, species_id = 0):
     variables = {'species': species,
                  'form': form, }
     return render(request, 'kineticmodels/species_editor.html', variables)
+    
+
 
 
 def kineticModel_list(request):
@@ -119,8 +129,32 @@ def kineticModel_list(request):
     See models.html
     """
     kineticModel_list = KineticModel.objects.all()
-    variables = {'kineticModel_list': kineticModel_list}
+
+    paginator = Paginator(kineticModel_list, ITEMSPERPAGE)
+
+    page = request.GET.get('page')
+    try:
+        kineticModelsOnAPage = paginator.page(page)
+    except PageNotAnInteger:
+        # If page is not an integer, deliver first page.
+        kineticModelsOnAPage = paginator.page(1)
+    except EmptyPage:
+        # If page is out of range (e.g. 9999), deliver last page of results.
+        kineticModelsOnAPage = paginator.page(paginator.num_pages)
+ 
+    variables = {'kineticModel_list': kineticModelsOnAPage}
     return render(request, 'kineticmodels/kineticModel_list.html', variables) 
+
+
+""" See kineticModel.html"""
+def kineticModel(request, kineticModel_id=0):
+    """
+    The listing of a specific kinetic Model in the database
+    """
+    kineticModel = get_object_or_404(KineticModel, id=kineticModel_id)
+    variables = {'kineticModel': kineticModel}
+    return render(request, 'kineticmodels/kineticModel.html', variables)
+
 
 def kineticModel_editor(request, kineticModel_id = 0):
     """
@@ -154,8 +188,31 @@ def reaction_list(request):
     See reactions.html
     """
     reaction_list = Reaction.objects.all()
-    variables = {'reaction_list': reaction_list}
+    
+    paginator = Paginator(reaction_list, ITEMSPERPAGE)
+
+    page = request.GET.get('page')
+    try:
+        reactionsOnAPage = paginator.page(page)
+    except PageNotAnInteger:
+        # If page is not an integer, deliver first page.
+        reactionsOnAPage = paginator.page(1)
+    except EmptyPage:
+        # If page is out of range (e.g. 9999), deliver last page of results.
+        reactionsOnAPage = paginator.page(paginator.num_pages)
+
+    variables = {'reaction_list': reactionsOnAPage}
     return render(request, 'kineticmodels/reaction_list.html', variables) 
+
+
+""" See reaction.html"""
+def reaction(request, reaction_id=0):
+    """
+    The listing of a specific reaction in the database
+    """
+    reaction = get_object_or_404(Reaction, id=reaction_id)
+    variables = {'reaction': reaction}
+    return render(request, 'kineticmodels/reaction.html', variables)
 
 def reaction_editor(request, reaction_id = 0):
     """
