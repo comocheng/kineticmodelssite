901190b562d2320a9de1768213069dde7b4d3b72
Hamza Khan <hkhan@g.hmc.edu>
2016-07-29 12:02:53 -0400

    Made changes to fix the bug regarding reading from the wrong folder.

--- data/media/kinetic_models/2/import.sh
+++ data/media/kinetic_models/2/import.sh
@@ -5,9 +5,9 @@
 #BSUB -n 1
 
 python -m cProfile -o importChemkin.profile $RMGpy/importChemkin.py  \
-	--species chemkin.txt \
-	--reactions chemkin.txt \
-	--thermo thermo.txt \
-	--known SMILES.txt \
+	--species data/media/kinetic_models/2/chemkin.txt \
+	--reactions data/media/kinetic_models/2/chemkin.txt \
+	--thermo data/media/kinetic_models/2/thermo.txt \
+	--known data/media/kinetic_models/2/SMILES.txt \
 	--port 8156
 gprof2dot -f pstats  importChemkin.profile | dot -Tpdf -o importChemkin.profile.pdf
