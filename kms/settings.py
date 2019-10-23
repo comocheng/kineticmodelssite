b23f3642074c86bbe7b4278c99b1c409c4607b87
kianmehrabani <kianmehrabani@gmail.com>
2019-10-22 22:49:47 -0400

    add django_filters to apps

--- kms/settings.py
+++ kms/settings.py
@@ -31,13 +31,14 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
-    'database.apps.DatabaseConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'django_filters',
+    'database.apps.DatabaseConfig',
 ]
 
 MIDDLEWARE = [
