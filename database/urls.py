543362c7c79aa196a72a37009ff70bf5fd163386
kianmehrabani <kianmehrabani@gmail.com>
2019-10-23 17:42:07 -0400

    feat: add species detail url

--- database/urls.py
+++ database/urls.py
@@ -5,4 +5,5 @@ from . import views
 
 urlpatterns = [
     path(r"species_search/", FilterView.as_view(filterset_class=views.SpeciesFilter), name="species-search"),
+    path(r"species_detail/<int:pk>", views.SpeciesDetail.as_view(), name="species-detail")
 ]
