92d485d5d16ad7bd43f54795e1bb2d45161f8057
Kian Mehrabani <kianmehrabani@gmail.com>
2020-10-12 22:02:57 -0400

    disable django media url when not in debug mode

--- kms/urls.py
+++ kms/urls.py
@@ -19,9 +19,14 @@ Including another URLconf
 """
 
 from django.contrib import admin
+from django.conf import settings
+from django.conf.urls.static import static
 from django.urls import path, include
 
 urlpatterns = [
     path("", include("database.urls")),
     path("admin/", admin.site.urls),
-] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
