fd1b0c41b1936622c786ec1398c7c3ac2849789f
Kian Mehrabani <kianmehrabani@gmail.com>
2020-10-12 22:07:15 -0400

    add nginx and remove whitenoise
    NGINX is a webserver that works in conjunction with gunicorn. In this
    case it is acting as a reverse-proxy. This is useful as we use NGINX to
    host static and media files. Because of this, we no longer need
    whitenoise for serving static files.

--- kms/settings.py
+++ kms/settings.py
@@ -35,7 +35,6 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
-    "whitenoise.runserver_nostatic",
     "django.contrib.admin",
     "django.contrib.auth",
     "django.contrib.contenttypes",
@@ -50,7 +49,6 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     "django.middleware.security.SecurityMiddleware",
-    "whitenoise.middleware.WhiteNoiseMiddleware",
     "django.contrib.sessions.middleware.SessionMiddleware",
     "django.middleware.common.CommonMiddleware",
     "django.middleware.csrf.CsrfViewMiddleware",
@@ -125,8 +123,6 @@ USE_TZ = True
 
 STATIC_ROOT = os.path.join(BASE_DIR, "static")
 STATIC_URL = "/static/"
-STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
-
 
 MEDIA_ROOT = os.path.join(BASE_DIR, "media")
 MEDIA_URL = "/media/"
