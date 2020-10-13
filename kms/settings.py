ccc13c2be65d55fa92ad0c551c5a9b0fb0c44b6b
Kian Mehrabani <kianmehrabani@gmail.com>
2020-10-12 22:06:42 -0400

    run django in debug if using .env.dev

--- kms/settings.py
+++ kms/settings.py
@@ -27,7 +27,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = os.getenv("DJANGO_SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.getenv("DEBUG") == "True"
 
 ALLOWED_HOSTS = []
 
