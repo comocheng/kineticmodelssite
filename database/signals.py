7a5cdf7bd5edaba298524e2bdaba23a5d142c7ee
Kian Mehrabani <kianmehrabani@gmail.com>
2020-12-06 23:30:09 -0500

    accept kwargs in signal functions

--- database/signals.py
+++ database/signals.py
@@ -5,12 +5,12 @@ from .scripts.import_rmg_models import get_species_hash, get_reaction_hash
 
 
 @receiver(m2m_changed, sender=Reaction.species.through)
-def change_reaction_hash_on_stoichiometry_change(instance):
+def change_reaction_hash_on_stoichiometry_change(instance, **kwargs):
     instance.hash = get_reaction_hash(instance.stoich_species())
     instance.save()
 
 
 @receiver(m2m_changed, sender=Species.isomers.through)
-def change_species_hash_on_isomers_change(instance):
+def change_species_hash_on_isomers_change(instance, **kwargs):
     instance.hash = get_species_hash(instance.isomers.all())
     instance.save()
