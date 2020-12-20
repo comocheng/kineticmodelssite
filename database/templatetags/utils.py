ab1ccff6debb4ea9cf06ba75361d8dfb2155d603
Kian Mehrabani <kianmehrabani@gmail.com>
2020-12-20 00:16:10 -0500

    move thermo table rendering to renders.py

--- database/templatetags/utils.py
+++ database/templatetags/utils.py
@@ -11,7 +11,7 @@ def param_replace(context, **kwargs):
     request's parameters, only with the specified GET parameters added or changed.
 
     It also removes any empty parameters to keep things neat,
-    so you can remove a parm by setting it to ``""``.
+    so you can remove a param by setting it to ``""``.
 
     For example, if you're on the page ``/things/?with_frosting=true&page=5``,
     then
@@ -44,60 +44,3 @@ def fields(obj):
 def pluralize(num, singular_word):
     word = singular_word if num == 1 else mark_safe(f"{singular_word}s")
     return f"{num} {word}"
-
-
-@register.filter
-def render_thermo_table(thermo, condensed=False):
-    return mark_safe(
-        """
-    <table class="table {condensed_class}">
-        <thead>
-            <tr>
-                <th scope="col">#</th>
-                <th scope="col">$T_{{min}}$ $(K)$</th>
-                <th scope="col">$T_{{max}}$ $(K)$</th>
-                <th scope="col">$a_0$</th>
-                <th scope="col">$a_1$</th>
-                <th scope="col">$a_2$</th>
-                <th scope="col">$a_3$</th>
-                <th scope="col">$a_4$</th>
-                <th scope="col">$a_5$</th>
-                <th scope="col">$a_6$</th>
-            </tr>
-        </thead>
-        <tbody>
-            <tr>
-                <th scope="row">1</th>
-                <td>{thermo.temp_min_1}</td>
-                <td>{thermo.temp_max_1}</td>
-                <td>{thermo.coeffs_poly1[0]}</td>
-                <td>{thermo.coeffs_poly1[1]}</td>
-                <td>{thermo.coeffs_poly1[2]}</td>
-                <td>{thermo.coeffs_poly1[3]}</td>
-                <td>{thermo.coeffs_poly1[4]}</td>
-                <td>{thermo.coeffs_poly1[5]}</td>
-                <td>{thermo.coeffs_poly1[6]}</td>
-            </tr>
-            <tr>
-                <th scope="row">2</th>
-                <td>{thermo.temp_min_2}</td>
-                <td>{thermo.temp_max_2}</td>
-                <td>{thermo.coeffs_poly2[0]}</td>
-                <td>{thermo.coeffs_poly2[1]}</td>
-                <td>{thermo.coeffs_poly2[2]}</td>
-                <td>{thermo.coeffs_poly2[3]}</td>
-                <td>{thermo.coeffs_poly2[4]}</td>
-                <td>{thermo.coeffs_poly2[5]}</td>
-                <td>{thermo.coeffs_poly2[6]}</td>
-            </tr>
-        </tbody>
-    </table>
-    """.format(
-            thermo=thermo, condensed_class="table-condensed" if condensed else ""
-        )
-    )
-
-
-@register.filter
-def render_transport_table(transport, condensed=False):
-    return mark_safe("<table></table>")
