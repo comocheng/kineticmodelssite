e9e99d31e73f09ab49102708df2f553cb8a35126
Kian Mehrabani <kianmehrabani@gmail.com>
2020-12-20 17:04:38 -0500

    add script for running server regularly

--- bin/runserver.sh
+++ bin/runserver.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
 
-pip install debugpy -t /tmp
-python /tmp/debugpy --wait-for-client --listen 0.0.0.0:5678 manage.py runserver 0.0.0.0:8080 --nothreading
+python manage.py runserver 0.0.0.0:8080 --nothreading
