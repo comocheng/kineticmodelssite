074f42e9ec4b0762eadc9d67e6e68e7f2d62a6e8
Kian Mehrabani <kianmehrabani@gmail.com>
2021-01-10 00:23:13 -0500

    fix url name collision

--- api/urls.py
+++ api/urls.py
@@ -1,19 +1,16 @@
-from django.urls import path, include
 from rest_framework import routers
 
 from api import views
 
 
 router = routers.DefaultRouter()
-router.register(r"formula", views.FormulaViewSet)
-router.register(r"isomer", views.IsomerViewSet)
-router.register(r"species", views.SpeciesViewSet)
-router.register(r"reaction", views.ReactionViewSet)
-router.register(r"thermo", views.ThermoViewSet)
-router.register(r"transport", views.TransportViewSet)
-router.register(r"kinetics", views.KineticsViewSet)
-router.register(r"kineticmodel", views.KineticModelViewSet)
+router.register(r"formula", views.FormulaViewSet, basename="api-formula")
+router.register(r"isomer", views.IsomerViewSet, basename="api-isomer")
+router.register(r"species", views.SpeciesViewSet, basename="api-species")
+router.register(r"reaction", views.ReactionViewSet, basename="api-reaction")
+router.register(r"thermo", views.ThermoViewSet, basename="api-thermo")
+router.register(r"transport", views.TransportViewSet, basename="api-transport")
+router.register(r"kinetics", views.KineticsViewSet, basename="api-kinetics")
+router.register(r"kineticmodel", views.KineticModelViewSet, basename="api-kineticmodel")
 
-urlpatterns = [
-    path("", include(router.urls)),
-]
+urlpatterns = router.urls
