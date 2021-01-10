bf4bc187555fe2d266e83b7736b37da7fec2572d
Kian Mehrabani <kianmehrabani@gmail.com>
2021-01-10 02:17:20 -0500

    add basic throttling

--- kms/settings.py
+++ kms/settings.py
@@ -147,4 +147,9 @@ REST_FRAMEWORK = {
     "DEFAULT_PERMISSION_CLASSES": [
         "rest_framework.permissions.IsAuthenticatedOrReadOnly",
     ],
+    "DEFAULT_THROTTLE_CLASSES": [
+        "rest_framework.throttling.AnonRateThrottle",
+        "rest_framework.throttling.UserRateThrottle",
+    ],
+    "DEFAULT_THROTTLE_RATES": {"anon": "100/day", "user": "1000/day"},
 }
